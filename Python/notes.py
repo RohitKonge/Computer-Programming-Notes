c300b98be8f6fa420c9cc5baaf2a376f5ad9d1e8
RohitKonge <rohitkonge08@gmail.com>
2022-06-18 20:14:15 +0530

    Added notes for python
    Python Notes

---
+++ Python/notes.py
@@ -0,0 +1,116 @@
+"""   Jupyter Notebook
+
+--> Shift + Enter = Run Current Cell and Add new Cell
+
+--> Alt + Enter = Add New Cell
+
+
+
+"""
+
+"""
+
+      Python
+
+--> num=12
+    name = Rohit 
+    'my number is {} and name is{}'.format(num,name)
+    
+--> s = 'hello'
+    s[0] will give 'h'
+    
+--> Slicing     s[includes this : doesnt not includes this]
+
+    s[0:] will give 'hello'
+    s[:3] will give 'hell'
+    
+--> List
+ 
+    my_list = [1,2,3,4]   
+    my_list.append(5)  --> [1,2,3,4,5]
+    my_list[0]  -> 1
+    
+    List can also be Sliced
+
+    nesting --> nest = [1, 2, [3,4,'target']]
+    nest[2][2][3]  -> 'g'
+    
+    
+    --> LIST Comprehension
+       
+    out = [num**2 for num in my_list]   
+    
+-->  Tuples are immutable
+
+    my_tuple = (1,2,3)
+
+--> Dictionaries (Key Value Pairs)
+
+    d = {'key1':'value', 'key2': 123}
+    
+    d['key1'] == 'value'
+    
+    
+    We can put anything in place of the Value like list, Dictionaries or 
+    even any variables
+    
+--> Set 
+
+    s = {1,2,3}         contains ony unique elements
+    
+    set([1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3])
+    {1,2,3}
+
+--> == , !=, and, or
+
+--> if and elif
+
+    if 1<2:
+        print("hello")
+    elif 2>4:
+        print("laugh")
+    else :
+        print("jump")
+    
+    
+--> for loop
+
+    seq = [1,2,3,4,5]
+
+    for item in seq:
+        print(item)
+            
+--> while loop 
+
+    i = 1
+    
+    while i < 5:
+        print('i is {}'.format(i))
+        i = i + 1;
+        
+--> range()
+
+    range(2,8)
+    --> 2,3,4,5,6,7
+    
+    for x  in range(10):
+        print(x)
+
+--> Functions
+
+    def my_func(param1 = "Default Name"):
+        print(param1)
+
+    my_func()           --> will print 'Default Name'
+    my_func("Rohit")    --> will print "Rohit"
+
+
+        def my_func(sum):
+            return(sum**2)
+
+        my_func(6)
+
+
+
+
+""" 
