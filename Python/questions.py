92555c055008cfc43ebde14e72f25b99dcbb3ec3
Rohit Konge <rohitkonge08@gmail.com>
2022-07-08 14:33:11 +0530

    Added notes on Images with Python 2

--- Python/questions.py
+++ Python/questions.py
@@ -1,23 +1,36 @@
-import requests, bs4
+# import requests, bs4
 
-i = 1
-authors = set()
-while True:
-    try:
-            result = requests.get(("http://quotes.toscrape.com/page/{}/").format(i))
+# i = 1
+# authors = set()
+# while True:
+#     try:
+#             result = requests.get(("http://quotes.toscrape.com/page/{}/").format(i))
 
-            soup = bs4.BeautifulSoup(result.text, "lxml")
+#             soup = bs4.BeautifulSoup(result.text, "lxml")
 
-            for k in soup.select(".author"):
-                authors.add(k.text)
+#             for k in soup.select(".author"):
+#                 authors.add(k.text)
                 
-            for j in authors:
-                print(j)
+#             for j in authors:
+#                 print(j)
                 
-            i = i + 1
+#             i = i + 1
         
-    except:
-        print(result.text)
-        print("asdasf")
-        break
+#     except:
+#         print(result.text)
+#         print("asdasf")
+#         break
     
+from PIL import Image
+
+img_word_matrix = Image.open("Python/word_matrix.png")
+img_mask        = Image.open("Python/mask.png")
+
+img_mask =  img_mask.resize((1015,559))
+
+img_word_matrix.putalpha(50)
+
+img_mask.paste(im = img_word_matrix, box = (0,0),mask = img_word_matrix)
+
+img_mask.show()
+
